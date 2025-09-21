b07c30475aeaa1aa9f30fa2585124906986d7dc1
RemiZlatinis <remizlatinis@gmail.com>
2025-09-21 15:55:46 +0300

    feat(config): Load back-end URL form environment variable

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,9 @@
+const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
+
+if (!BACKEND_URL) throw Error("EXPO_PUBLIC_BACKEND_URL is not defined!");
+
 const BACKEND = {
-  BASE_URL: "http://192.168.1.20:8000/",
+  BASE_URL: BACKEND_URL,
   LOGIN: "api/auth/login/",
   AGENTS: "api/agents/",
   AGENTS_SSE: "api/sse/agents/",
