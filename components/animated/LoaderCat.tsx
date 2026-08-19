2dd260b43d116ce3c171c3def85d1868b930b196
RemiZlatinis <remizlatinis@gmail.com>
2026-08-19 05:24:42 +0300

    feat(ui): add Mufasa as loader

--- components/animated/LoaderCat.tsx
+++ components/animated/LoaderCat.tsx
@@ -1,7 +1,7 @@
 import LottieView from "lottie-react-native";
 import { useRef } from "react";
 
-const loaderCat = require("@/assets/lottie/Loader cat.json");
+const loaderCat = require("@/assets/lottie/Mufasa.json");
 
 function LoaderCat() {
   const animation = useRef<LottieView>(null);
